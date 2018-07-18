39b0def19e633813e75c2c00d4a3977a4a5b0c89
Paolo Monella <paolo.monella@gmx.net>
2018-07-18 22:56:32 +0200

    Functions to check rs and hi

--- python/names.py
+++ python/names.py
@@ -27,8 +27,14 @@ def nameset (properNamesInputXmlFile, mytag='rs', addrstxt=True):
     #rss = names_tree.findall('.//t:rs', constants.ns)  # Old version
     mynames = names_tree.findall('.//t:%s' % (mytag), constants.ns)
     namelist = [rs.text.lower() for rs in mynames if len(list(rs)) == 0 ]   # The 'if' excludes <rs>'s with element children
+    for m in mynames:   # This takes care of <rs>'s with element children
+        if len(list(m)) > 0:
+            if m.find('.//t:orig', constants.ns) is not None:   # Remove 'orig' (i.e. only leave 'reg': 'jovem' becomes 'iouem')
+                orig = m.find('.//t:orig', constants.ns)
+                orig.getparent().remove(orig)
+            namelist.append(''.join(m.itertext()).replace('\n', ''))
     if addrstxt:
-        with open ('rs.txt', 'r') as rstxt:
+        with open ('rs.txt', 'r') as rstxt: # Add the names stored in file 'rs.txt'
             for l in rstxt:
                 namelist.append(l.strip().lower())
     return set(namelist)
@@ -81,13 +87,13 @@ def updatenamesfile (updateProperNamesInputXmlFile):
             if n.strip() not in nn:
                 print(n.strip(), file=f)
 
-def checkrs (myInputXmlFile, myProperNamesFile):
-    ''' Update the XML file based on the <rs>'s in the XML file 'myInputXmlFile'.
+def checkrs_old (myInputXmlFile, myProperNamesFile):
+    ''' Update file rs.txt based on the <rs>'s in the XML file 'myInputXmlFile'.
         Then parse 'myInputXmlFile'
         perform a textual search on the XML file for each proper name
         listed in file myProperNamesFile, and check that each of the occurrences of each name
         in the XML files is marked with a <rs> tag.
-        If this is not the case, the print out the XML file name,
+        If this is not the case, then print out the XML file name,
         the parent element that includes the name, and that element's text. '''
     updatenamesfile(myInputXmlFile)
     names_tree = etree.parse(myInputXmlFile)
@@ -101,5 +107,51 @@ def checkrs (myInputXmlFile, myProperNamesFile):
                 if r.tag != constants.tei_ns + 'rs':    # If the proper name is not marked with <rs>
                     print('%10s %s %10s %s %10s %s' % ('File:', myInputXmlFile, 'Name:', myname, 'Tag:', r.tag))
 
+def checkrs (myInputXmlFile, setonly=True):
+    ''' Parse 'myInputXmlFile' and get a set of names marked with <rs>. Then
+        perform a textual search on the XML file for each proper name
+        listed in file myProperNamesFile, and check that each of the occurrences of each name
+        in the XML files is marked with a <rs> tag.
+        If this is not the case, then print out the XML file name,
+        the parent element that includes the name, and that element's text.
+        If setonly=True, don't show each occurrence of the unmarked word, but a set in which
+        each word only occurs once.
+        '''
+    rsset = nameset(myInputXmlFile, mytag='rs', addrstxt=False)
+    names_tree = etree.parse(myInputXmlFile)
+    mybody = names_tree.find('.//t:body', constants.ns)
+    regexpNS = 'http://exslt.org/regular-expressions'
+    if not setonly:
+        print('\n---\n\nNames marked with <rs> at some point of the text, but not marked in other points:')
+    unmarked = []
+    for myname in rsset:
+        #myname = l.strip().lower()
+        find = etree.XPath('//text()[re:match(., "\W%s\W", "i")]/parent::*' % (myname), namespaces={'re':regexpNS})
+        # Doc: http://exslt.org/regexp/ e http://exslt.org/regexp/functions/test/index.html
+        #for r in find(names_tree):
+        for r in find(mybody):
+            if r.tag != constants.tei_ns + 'rs':    # If the proper name is not marked with <rs>
+                if setonly:
+                    unmarked.append(myname)
+                else:
+                    simpletag = r.tag.split('}')[1]
+                    print('%10s %s %10s %s %10s %s' % ('File:', myInputXmlFile, 'Name:', myname, 'Parent tag:', simpletag))
+    if setonly:
+        print(set(unmarked))
 
-
+def checkrsandhi (myInputXmlFile):
+    '''Create a set of words currently marked with <rs> and another list of words
+        currently marked with <hi>. Check that words marked <rs> are never marked as <hi>
+        in other points of the text, and the other way around.
+        '''
+    okwords = {
+            # Words that are OK to be marked both as <hi> and as <rs> in a specific file
+            '../xml/bonetti.xml': ['domine', 'dominus'],
+            '../xml/a.xml': [],
+            '../xml/g.xml': []
+            }
+    rsset = nameset(myInputXmlFile, mytag='rs', addrstxt=False)
+    hiset = nameset(myInputXmlFile, mytag='hi', addrstxt=False)
+    for r in rsset:
+        if r in hiset and not r in okwords[myInputXmlFile]:
+            print('Word "' + r + '" is marked with <rs>, but elsewhere also with <hi>')
