4072f6c7b4c1b6fab2ab5abcdb762019fcd0de68
Paolo Monella <paolo.monella@gmx.net>
2020-02-21 21:53:45 +0100

    Add substitutions; replace dict with list

--- python/entitize.py
+++ python/entitize.py
@@ -5,14 +5,25 @@ import time
 import os
 from glob import iglob
 
-ent = {
+ent_old = {
         'gd': '<lb xmlns="http://www.tei-c.org/ns/1.0" break="no" rend="-" ed="#g"/>',
-        'glb': '<lb xmlns="http://www.tei-c.org/ns/1.0" ed="#g"/>',
+        'gl': '<lb xmlns="http://www.tei-c.org/ns/1.0" ed="#g"/>',
         'bd': '<lb xmlns="http://www.tei-c.org/ns/1.0" break="no" rend="-" ed="#b"/>',
-        'blb': '<lb xmlns="http://www.tei-c.org/ns/1.0" ed="#b"/>',
+        'bl': '<lb xmlns="http://www.tei-c.org/ns/1.0" ed="#b"/>',
         'jj': '<choice xmlns="http://www.tei-c.org/ns/1.0"><orig xmlns="http://www.tei-c.org/ns/1.0">j</orig><reg xmlns="http://www.tei-c.org/ns/1.0" type="j">i</reg></choice>',
         'uu': '<choice xmlns="http://www.tei-c.org/ns/1.0"><orig xmlns="http://www.tei-c.org/ns/1.0">v</orig><reg xmlns="http://www.tei-c.org/ns/1.0" type="v">u</reg></choice>',
         }
+
+ent = [
+        ['<lb xmlns="http://www.tei-c.org/ns/1.0" break="no" rend="-" ed="#g"/>', 'gd'],
+        ['<lb xmlns="http://www.tei-c.org/ns/1.0" ed="#g"/>', 'gl'],
+        ['<lb xmlns="http://www.tei-c.org/ns/1.0" break="no" rend="-" ed="#b"/>', 'bd'],
+        ['<lb break="no" rend="-" ed="#b"/>', 'bd'],
+        ['<lb xmlns="http://www.tei-c.org/ns/1.0" ed="#b"/>', 'bl'],
+        ['<lb ed="#b"/>', 'bl'],
+        ['<choice xmlns="http://www.tei-c.org/ns/1.0"><orig xmlns="http://www.tei-c.org/ns/1.0">j</orig><reg xmlns="http://www.tei-c.org/ns/1.0" type="j">i</reg></choice>', 'jj'],
+        ['<choice xmlns="http://www.tei-c.org/ns/1.0"><orig xmlns="http://www.tei-c.org/ns/1.0">v</orig><reg xmlns="http://www.tei-c.org/ns/1.0" type="v">u</reg></choice>', 'uu'],
+        ]
 
 def entitize (filename):
     ''' Replace long tags with their corresponding entities '''
@@ -22,7 +33,8 @@ def entitize (filename):
     input_filename = ''.join(['../xml/', filename, '.xml'])
     #backup_filename = '_'.join([datetime, ms, 'id-spreading-backup.xml'])
     #backup_filename = input_filename.replace('.xml', '_backup_before_entitization_' + datetime + '.xml')
-    backup_filename = ''.join(['../xml/backup/backup_entitization/', datetime, '_backup_before_entitization_of_', filename, '.xml'])
+    backup_filename = ''.join(['../xml/ripostiglio/backup/backup_entitization/',
+        datetime, '_backup_before_entitization_of_', filename, '.xml'])
     os.system('cp %s %s' % (input_filename, backup_filename)) # Create backup of old file
 
     with open (input_filename, 'r') as IN:
@@ -33,7 +45,7 @@ def entitize (filename):
                     pass
                 else:
                     for e in ent:
-                        l = l.replace(ent[e], '&%s;' % (e))
+                        l = l.replace(e[0], '&%s;' % (e[1]))
                 print(l, file=OUT, end='')
 
     os.system('mv %s %s' % (output_filename, input_filename)) # Overwrite original file
