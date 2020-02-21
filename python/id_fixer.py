ca238f692741b17c247b78f1dd60b365ce04a8c8
Paolo Monella <paolo.monella@gmx.net>
2020-02-21 21:55:04 +0100

    Add function to compare xml:ids

--- python/id_fixer.py
+++ python/id_fixer.py
@@ -6,27 +6,42 @@ from lxml import etree
 import time
 import os
 
-def compare_ids (wit1, wit2):
-    ''' This function takes xml:id's from XML file "wit1" and
-        compares them those of file "wit2"
-        '''
-    D = {}
-    for wit in [wit1, wit2]:
-        tree = etree.parse('../xml/%s.xml' % wit)
-        body = tree.find('.//t:body', myconst.ns)
-        pp = body.findall('.//t:p', myconst.ns)
-        D[wit] = [p.get(myconst.xml_ns + 'id') for p in pp]
+
+def compare_two_id_lists (wit1, wit2, D, orderMatters):
+    ''' The first two arguments are the sigla of the two witnesses.
+         What matters is the order in which the two args are given.
+         Argument D is the dictionary created in function compare_ids'''
     for xmlid1 in D[wit1]:
         index = D[wit1].index(xmlid1)
         try:
             xmlid2 = D[wit2][index]
             if xmlid1 != xmlid2:
-                print('%s: %15s %10s: %15s' % (wit1, xmlid1, wit2, xmlid2) )
+                if orderMatters:
+                    print('%s → %20s: %15s %10s: %15s' % ('Previous IDs', wit1, previousId1, wit2, previousId2) )
+                print('%s → %11s: %15s %10s: %15s' % ('Non-corresponding IDs', wit1, xmlid1, wit2, xmlid2) )
                 break
         except IndexError:
-            print('List index %d out of range. Witness "%s.xml" has xml:id %s' % (index, wit1, xmlid1)  )
+            print('List index %d out of range. Witness %s.xml has xml:id %s, while witness %s doesn\'t' % (index, wit1, xmlid1, wit2))
             break
-    
+        if orderMatters:
+            previousId1, previousId2 = xmlid1, xmlid2
+
+def compare_ids (witOne, witTwo, orderMatters = True):
+    ''' This function takes xml:id's from XML file "witOne" and
+        compares them those of file "witTwo"
+        '''
+    myDict = {}
+    for wit in [witOne, witTwo]:
+        tree = etree.parse('../xml/%s.xml' % wit)
+        body = tree.find('.//t:body', myconst.ns)
+        pp = body.findall('.//t:p', myconst.ns)
+        myDict[wit] = [p.get(myconst.xml_ns + 'id') for p in pp]
+        if not orderMatters:
+            myDict[wit] = sorted(myDict[wit])
+    print('%s → %14s: %15s %10s: %15s' % ('Number of xml:id\'s', witOne, len(myDict[witOne]), witTwo, len(myDict[witTwo])) )
+    compare_two_id_lists(witOne, witTwo, myDict, orderMatters)
+    compare_two_id_lists(witTwo, witOne, myDict, orderMatters)
+
 
 def spread_ids (get_p_ids_from, append_p_ids_to):
     ''' This function takes xml:id's from XML file "get_p_ids_from"
@@ -62,7 +77,9 @@ def spread_ids (get_p_ids_from, append_p_ids_to):
         output_tree.write('../xml/%s.xml' % (ms), encoding='UTF-8', method='xml', pretty_print=True, xml_declaration=True)
 
 
-#spread_ids ('a', 'b')
-#spread_ids ('a', 'c')
-#compare_ids ('a-tagliato', 'g')    # tutto OK
-compare_ids ('a-bonetti', 'bonetti')
+spread_ids ('a', 'b')
+spread_ids ('a', 'c')
+#compare_ids ('a', 'geb', orderMatters = False) # Quindi g + bonetti corrisponde ad a
+#compare_ids ('a1', 'g', orderMatters = True)   # OK prima-parte-a / g (non ci sono trasposizioni)
+#compare_ids ('a2', 'bonetti', orderMatters = False) # OK seconda-parte-a / bonetti (ci sono trasposizioni, però)
+#compare_ids ('o', 'bonetti', orderMatters = True) # OK seconda-parte-a / bonetti (ci sono trasposizioni, però)
