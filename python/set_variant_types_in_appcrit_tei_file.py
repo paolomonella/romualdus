383be1d7768955c1207dd0609303e166da9506ff
Paolo Monella <paolo.monella@gmx.net>
2020-03-01 00:38:18 +0100

    Detect more variant types

--- python/set_variant_types_in_appcrit_tei_file.py
+++ python/set_variant_types_in_appcrit_tei_file.py
@@ -11,76 +11,127 @@ from variant_type import variantComparison
 
 debug = False
 
+class treeWithAppElements:
+
+    def __init__ (self, myXmlFile, printSiglum, msSiglum):
+        self.printSiglum, self.msSiglum = printSiglum, msSiglum
+        self.tree = etree.parse(myXmlFile)
+        self.apps = self.tree.findall('.//t:app', ns)
+        self.outputXmlFile = myXmlFile.replace('.xml', '-out.xml')
+
+    def appComparisonList (self):
+        ''' Arguments:  printSiglum is the siglum (e.g. 'g' or 'bonetti') of the first witness (that's normally the print edition)
+                        msSiglum is the siglum (e.g. 'a' or 'o') of the 2nd witness (normally a MS)
+            The function parses file myXmlFile, finds all <app> elements, then
+            creates and populates 'comparisons', a list of dictionaries (one dict for each <app> in the TEI XML file).
+            Each dict in the list is the result of the comparison of two <app> TEI XML elements, and
+            has those keys (examples are for variants "sillaba" vs "syllaba"):
+                'r1' = the variant characters in myString1 ('y'), inherited from function variantComparison
+                'r2' = the variant characters in myString2 ('i'), inherited from function variantComparison
+                'type' = the variant type ('yType'), inherited from function variantComparison
+                plus new additional keys:
+                'app' = the <app> XML element
+                'printReading' = the <rdg> or <lem> XML element of the 1st witness (corresp. to printSiglum)
+                'msReading' = the <rdg> or <lem> XML element of the 2nd witness (corresp. to msSiglum)
+                'printText' = the text (string) of the variant of the 1st witness (corresp. to printSiglum)
+                'msText' = the text (string) of the variant of the 2nd witness (corresp. to printSiglum)
+        '''
+
+        comparisons = []    
+        for app in self.apps:
+            printReading = app.find('.//t:*[@wit="#%s"]' % (self.printSiglum), ns)
+            msReading = app.find('.//t:*[@wit="#%s"]' % (self.msSiglum), ns)
+            if msReading is None:
+                print(printReading.text)
+            if printReading.text is not None:
+                printText = printReading.text
+            else:
+                printText = ''
+            if msReading.text is not None:
+                msText = msReading.text
+            else:
+                msText = ''
+            if debug:
+                print('\n\nprintText: «%s»' % (printText))
+                print('msText: «%s»' % (msText))
+
+            myComp = variantComparison(printText, msText) # A dict. Note that the 1st one is the Garufi text; the 2nd is MS A text
+            myComp['app'] = app
+            myComp['printReading'] = printReading
+            myComp['msReading'] = msReading
+            myComp['printText'] = printText
+            myComp['msText'] = msText
+            comparisons.append(myComp)
+        return comparisons
 
 
-def appComparisonList (myXmlFile, printSiglum, msSiglum):
-    ''' Arguments:  printSiglum is the siglum (e.g. 'g' or 'bonetti') of the first witness (that's normally the print edition)
-                    msSiglum is the siglum (e.g. 'a' or 'o') of the 2nd witness (normally a MS)
-        The function parses file myXmlFile, finds all <app> elements, then
-        creates and populates 'comparisons', a list of dictionaries (one dict for each <app> in the TEI XML file).
-        Each dict in the list is the result of the comparison of two <app> TEI XML elements, and
-        has those keys (examples are for variants "sillaba" vs "syllaba"):
-            "r1" = the variant characters in myString1 ("y"), inherited from function variantComparison
-            "r2" = the variant characters in myString2 ("i"), inherited from function variantComparison
-            "type" = the variant type ("yType"), inherited from function variantComparison
-            plus new additional keys:
-            "app" = the <app> XML element
-            "printReading" = the <rdg> or <lem> XML element of the 1st witness (corresp. to printSiglum)
-            "msReading" = the <rdg> or <lem> XML element of the 2nd witness (corresp. to msSiglum)
-            "printText" = the text (string) of the variant of the 1st witness (corresp. to printSiglum)
-            "msText" = the text (string) of the variant of the 2nd witness (corresp. to printSiglum)
-    '''
+    def variantTypesList (self):
+        '''List all variant types in <app> '''
+        myList = [c['type'] for c in self.appComparisonList()]
+        mySet = [t for t in set(myList)]
+        return mySet
 
-    tree = etree.parse(myXmlFile)
-    apps = tree.findall('.//t:app', ns)
 
-    comparisons = []    
-    for app in apps:
-        printReading = app.find('.//t:*[@wit="#%s"]' % (printSiglum), ns)
-        msReading = app.find('.//t:*[@wit="#%s"]' % (msSiglum), ns)
-        if printReading.text is not None:
-            printText = printReading.text
-        else:
-            printText = ''
-        if msReading.text is not None:
-            msText = msReading.text
-        else:
-            msText = ''
-        if debug:
-            print('\n\nprintText: «%s»' % (printText))
-            print('msText: «%s»' % (msText))
-
-        myComp = variantComparison(printText, msText) # A dict. Note that the 1st one is the Garufi text; the 2nd is MS A text
-        myComp['app'] = app
-        myComp['printReading'] = printReading
-        myComp['msReading'] = msReading
-        myComp['printText'] = printText
-        myComp['msText'] = msText
-        comparisons.append(myComp)
-    return comparisons
-
-def countVariantTypes(myXmlFile, printSiglum, msSiglum, countedType):
-    '''Count types of variants in a comparisonList. The 1st two arguments are two sigla (put print witness first).
-        The 3rd arguments is the type of variant (e.g. "yType") one particularly wants to count '''
-    typedList = []
-    untypedList = []
-    countedTypeList = []
-    for c in appComparisonList(myXmlFile, printSiglum, msSiglum):
-        if c['type'] != 'unknown':
-            typedList.append(c)
-        else:
-            untypedList.append(c)
+    def setTypeAttributesForApps (self):
+        '''Set @type attributes in <app> elements in the input TEI XML file '''
+        for c in self.appComparisonList():
+            c['app'].set('type', c['type'])
             if debug:
                 print('\n')
+                print('«%s» | «%s» %15s @type="%s"' % (c['printText'], c['msText'], '·', c['type']))
                 for k in c:
                     print('%s: «%s»' % (k, c[k]))
 
-        if c['type'] == countedType:
-            countedTypeList.append(c)
-
-    print('typed:', len(typedList))
-    print('untyped:', len(untypedList))
-    print('%s:' % (countedType), len(countedTypeList))
+    def setLems (self):
+        '''For some @type(s) of <app>, decide the <lem> automatically '''
 
 
-countVariantTypes('../xml/m.xml', 'g', 'a', 'num-numType')
+        decisionTable = {
+                'num-WordType': 'printReading',
+                'differentPunctType': 'msReading',   #confirmed
+                'unknown': 'printReading',
+                'missingInPrintType': 'printReading',
+                'num-numType': 'printReading',
+                'punctInPrint-punctAndLettersInMS-Type': 'printReading',
+                'caseType': 'printReading',
+                'missingInMSType': 'printReading',
+                'missingInPrint-PunctInMS-Type': 'printReading',
+                'punctInPrint-missingInMS-Type': 'printReading',
+                'nichilType': 'printReading',
+                }
+        for c in self.appComparisonList():
+            for t in decisionTable:
+                if c['type'] == t:
+                    c[decisionTable[t]].tag = 'lem'
+
+    def countVariantTypes (self, countedType):
+        '''Count types of variants in a comparisonList. The 1st two arguments are two sigla (put print witness first).
+            The 3rd arguments is the type of variant (e.g. "yType") one particularly wants to count '''
+        typedList = []
+        untypedList = []
+        countedTypeList = []
+        for c in self.appComparisonList():
+            if c['type'] != 'unknown':
+                typedList.append(c)
+            else:
+                untypedList.append(c)
+                if debug:
+                    print('\n')
+                    for k in c:
+                        print('%s: «%s»' % (k, c[k]))
+
+            if c['type'] == countedType:
+                countedTypeList.append(c)
+
+        print('\nTyped:', len(typedList))
+        print('Untyped:', len(untypedList))
+        print('Type "%s":' % (countedType), len(countedTypeList))
+
+    def write (self):
+        self.tree.write(self.outputXmlFile, encoding='UTF-8', method='xml', pretty_print=True, xml_declaration=True)
+
+myTree = treeWithAppElements('../xml/m.xml', 'g', 'a')
+myTree.countVariantTypes('unknown')
+myTree.setTypeAttributesForApps()
+myTree.setLems()
+myTree.write()
--- python/variant_type.py
+++ python/variant_type.py
@@ -3,7 +3,7 @@
 
 ''' This module manages strings (e.g. "syllaba" vs "sillaba"), not <app> or other TEI XML elements'''
 
-import roman
+import roman, re
 from lxml import etree
 from myconst import ns
 
@@ -53,10 +53,15 @@ def numeralCheck(myStringOrig):
 def getVariantTypeBasedOnWholeVariant (myString1, myString2):
     ''' Take two strings (two variants) and try to detect the variant type based on the whole variant
         '''
+    # Create my list of punctuation characters
+    myPunctString = '!"()*+,-.:;=?^_`{|}~' + "'"     # long version: it generates 210 combinations
 
-    myType = 'unknown'
+    #myPunctString = ',.:;?!"()-' + "'"     # it generates 55 combinations
+    myPunctList = [p for p in myPunctString]  # transform the string to a list
+    #punctCombList = [   [c[0], c[1], 'punct']   for c in combinations(myPunctList, 2)]  # it is a list of lists
+
+    # Detect case 'VIII' / 'IX' (case-insensitive)
     if numeralCheck(myString1.strip())['isNumeral'] and numeralCheck(myString2.strip())['isNumeral']:
-        # Detect case 'VIII' / 'IX' (case-insensitive)
         if debug: print('\n«%s»     «%s»' % (myString1, myString2))
         myType = 'num-numType'
     elif numeralCheck(myString1.strip())['isNumeral']:
@@ -67,14 +72,75 @@ def getVariantTypeBasedOnWholeVariant (myString1, myString2):
             if myWord == myString2.strip().lower(): # If the other variant corresponds to one of those words
                 if debug: print('EUREKA! This is a word/non-word numeral variant!')
                 myType = 'num-WordType'
+            else:
+                myType = 'unknown'
+
+    # Detect case 'octo' / 'VIII' (case-insensitive)
     elif numeralCheck(myString2.strip())['isNumeral']:
-        # Detect case 'octo' / 'VIII' (case-insensitive)
         if debug: print('\n«%s»     «%s»      «%s»' % 
                 (myString1, myString2, numeralCheck(myString1.strip())['words']))
         for myWord in numeralCheck(myString2.strip())['words']: # Check the corresponding words, e.g.: ['octo', 'octauo']
             if myWord == myString1.strip().lower(): # If the other variant corresponds to one of those words
                 if debug: print('EUREKA! This is a word/non-word numeral variant!')
                 myType = 'num-WordType'
+            else:
+                myType = 'unknown'
+
+    elif myString1 != myString2 and myString1.lower() == myString2.lower():
+        if debug: print(myString1.strip(), myString2.strip())
+        myType = 'caseType'
+
+    elif myString1.strip() == '' and myString2.strip() in myPunctList:    # bring to wholeVariant
+        myType = 'missingInPrint-PunctInMS-Type'
+
+    elif myString1.strip() in myPunctList and myString2.strip() == '':  # bring to wholeVariant
+        myType = 'punctInPrint-missingInMS-Type'
+
+    elif myString1.strip() == '' and myString2.strip() not in myPunctList and myString2.strip() != '':  # bring to wholeVariant
+        myType = 'missingInPrintType'
+
+    elif myString1.strip() not in myPunctList and myString1.strip() != '' and myString2.strip() == '':  # bring to wholeVariant
+        myType = 'missingInMSType'
+
+    elif myString1.strip() in myPunctList and myString2.strip() in myPunctList:  # bring to wholeVariant
+        myType = 'differentPunctType'
+
+    # If first variant is only a punctuation sign and the second variant has a punct plus at least one letter
+    elif myString1.strip() in myPunctList \
+            and any((j in myPunctString) for j in myString2.strip()) \
+            and any(j.isalpha() for j in myString2.strip()):
+        myType = 'punctInPrint-punctAndLettersInMS-Type'
+
+    # The same, the other way around
+    elif myString2.strip() in myPunctList \
+            and any((j in myPunctString) for j in myString1.strip()) \
+            and any(j.isalpha() for j in myString1.strip()):
+        myType = 'punctAndLettersInPrint-punctInMS-Type'
+
+    elif \
+            any((j in myPunctString) for j in myString1) \
+            and any(j.isalpha() for j in myString1) \
+            \
+            and any((j in myPunctString) for j in myString2) \
+            and any(j.isalpha() for j in myString2):
+            # If both variants include a punct plus at least one letter.
+            # But I'm only interested in the sub-case described by the next 'if'
+
+        if myString1[0] in myPunctString and myString2[0] in myPunctString \
+                and myString1[1] == myString2[1] == ' ' \
+                and myString1[0] != myString2[0] \
+                and myString1[2:].lower() == myString2[2:].lower():
+                # Case «. Qui» | «, qui»    or    «, qui» ! «. Qui» (i.e.: if both variants
+                # start with a punctuation char followed by a space and the two punct chars are different)
+            if debug: print('%s «%s» | «%s» %30s «%s» | «%s»' %
+                    ('Strings:', myString1, myString2, 'Stripped strings:',  myString1.strip(), myString2.strip()))
+            myType = 'differentPunctType'
+        else:
+            myType = 'unknown'
+
+    else:
+        myType = 'unknown'
+
 
     return myType
 
@@ -101,9 +167,10 @@ def getDiff (myString1, myString2):
     return [result1, result2]
 
 
-def getVariantTypeBasedOnDiff (myDiffList):
+def getVariantTypeBasedOnDiff (myDiffList, myString1, myString2):
     ''' Input a list with two strings constituting the differences b/w two variants, e.g. input ['i', 'y']
-        (from variants ille/ylle) and evaluate the type of variant. '''
+        (from variants ille/ylle) and evaluate the type of variant.
+        I am also adding two more arguments (myString1, myString2) for some checks on the function. '''
 
     myDiff1, myDiff2 = myDiffList[0], myDiffList[1]
 
@@ -129,44 +196,52 @@ def getVariantTypeBasedOnDiff (myDiffList):
             ['hil', 'chil', 'nichil'],
             ]
 
+    '''
+    # start
     myPunctString = '!"()*+,-.:;=?^_`{|}~' + "'"     # long version: it generates 210 combinations
     #myPunctString = ',.:;?!"()-' + "'"     # it generates 55 combinations
     myPunctList = [p for p in myPunctString]  # transform the string to a list
     #punctCombList = [   [c[0], c[1], 'punct']   for c in combinations(myPunctList, 2)]  # it is a list of lists
 
-    if myDiff1.strip() == '' and myDiff2.strip() in myPunctList:
-        #print('«%s» | «%s» | «%s»'  % (myDiff1.strip(), myDiff2.strip(), myPunctString)  )
+    if myDiff1.strip() == '' and myDiff2.strip() in myPunctList:    # bring to wholeVariant
         myType = 'missingInG-PunctInA-Type'
 
-    elif myDiff2.strip() == '' and myDiff1.strip() in myPunctList:
+    elif myDiff2.strip() == '' and myDiff1.strip() in myPunctList:  # bring to wholeVariant
         myType = 'missingInA-PunctInG-Type'
 
-    elif myDiff1.strip() == '' and myDiff2.strip() not in myPunctList and myDiff2.strip() != '':
+    elif myDiff1.strip() == '' and myDiff2.strip() not in myPunctList and myDiff2.strip() != '':  # bring to wholeVariant
         myType = 'missingInGType'
 
-    elif myDiff2.strip() == '' and myDiff1.strip() not in myPunctList and myDiff1.strip() != '':
+    elif myDiff2.strip() == '' and myDiff1.strip() not in myPunctList and myDiff1.strip() != '':  # bring to wholeVariant
         myType = 'missingInAType'
 
-    elif myDiff1.strip()in myPunctList and myDiff2.strip() in myPunctList:
+    elif myDiff1.strip()in myPunctList and myDiff2.strip() in myPunctList:  # bring to wholeVariant
         myType = 'differentPunctType'
 
-    elif myDiff1.strip() in myPunctList and myDiff2.strip() not in myPunctList and myDiff2.strip() != '':
+    elif myDiff1.strip() in myPunctList and myDiff2.strip() not in myPunctList and myDiff2.strip() != '': #bring to wholeVariant & edit
+        print('Diffs: «%s» / «%s» -- Strings: «%s» / «%s»'   % (myDiff1.strip(), myDiff2.strip(), myString1, myString2)  )
         myType = 'punctInG-lettersInA-Type'
 
-    elif myDiff2.strip() in myPunctList and myDiff1.strip() not in myPunctList and myDiff1.strip() != '':
+    elif myDiff2.strip() in myPunctList and myDiff1.strip() not in myPunctList and myDiff1.strip() != '': #bring to wholeVariant & edit
         myType = 'lettersInG-punctInA-Type'
+    # end
 
-    elif myDiff1.strip().lower() == myDiff2.strip().lower():
+    if myDiff1 != myDiff2 and myDiff1.lower() == myDiff2.lower():
+        if debug: print(myDiff1.strip(), myDiff2.strip())
         myType = 'caseType'
     else:
         myType = 'unknown'
+    '''
 
+    myType = 'unknown'
     for t in typeList:
         if sorted([myDiff1, myDiff2]) == sorted([t[0], t[1]]): # sorted() makes the order of diffs in the MSS irrelevant
             if myType == 'unknown':
                 myType = '%sType' % (t[2])
             else:
                 print('Error! Diff «%s»/«%s» matched two different types: %s and %sType' % (myDiff1, myDiff2, myType, t[2]))
+        else:
+            myType = 'unknown'
 
     return(myType)
 
@@ -188,6 +263,7 @@ def variantComparison(myString1, myString2):
         myVariantType = getVariantTypeBasedOnWholeVariant(myString1, myString2)
     else:
         # If function getVariantTypeBasedOnWholeVariant doesn't detect a variant type, try detecting one based on diff
-        myVariantType = getVariantTypeBasedOnDiff(myDiff)
+        #print('Diffs: «%s» -- Strings: «%s» / «%s»'   % (myDiff, myString1, myString1)  )
+        myVariantType = getVariantTypeBasedOnDiff(myDiff, myString1, myString2)
 
     return({'r1': myDiff[0], 'r2': myDiff[1], 'type': myVariantType})
