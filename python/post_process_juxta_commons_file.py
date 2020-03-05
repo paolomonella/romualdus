2c1fcfb0fc820ff4fb1c620d9bb8159ee7c2d3ba
Paolo Monella <paolo.monella@gmx.net>
2020-03-06 00:26:15 +0100

    Make PEP8 compliant

--- python/post_process_juxta_commons_file.py
+++ python/post_process_juxta_commons_file.py
@@ -1,52 +1,76 @@
 #!/usr/bin/python3.6
 # -*- coding: utf-8 -*-
-''' Post-process the output of JuxtaCommons before inspecting variant types: see documentations of module(s) below '''
+''' Post-process the output of JuxtaCommons before inspecting variant types:
+    see documentations of module(s) below '''
 
 import myconst
 from simplify_markup_for_collation import msTree
 
-def postProcessJuxtaCommonsFile (siglum, printEdition = 'garufi', printSiglum = 'g', msSiglum='a'):
+
+def postProcessJuxtaCommonsFile(siglum, printEdition='garufi',
+                                printSiglum='g', msSiglum='a'):
     ''' Change
             - [tag attr="value"] to \n<tag attr="value">
             - [/tag] to \n</tag>
         Substitute
             - JuxtaComons sigla with my sigla ('g', 'a', 'b' etc.)
         '''
-    xmlfile = '%s/%s.xml' % (myconst.xmlpath, siglum)
+
+    ###########################
+    # REPLACE POINTY BRACKETS #
+    ###########################
+
+    xmlfile = '%s%s.xml' % (myconst.xmlpath, siglum)
     with open(xmlfile, 'r') as IN:
         myLines = []
         for line in IN:
             line = line.replace(']\n', ']')
-            line = line.replace('[', '\n<').replace(']', '>\n').replace('&quot;', '"')
+            line = line.replace('[', '\n<').replace(']', '>\n')
+            line = line.replace('&quot;', '"')
             myLines.append(line)
     with open(xmlfile, 'w') as OUT:
         for line in myLines:
             print(line, file=OUT, end='')
 
+    #############
+    # FIX SIGLA #
+    #############
+
     # Parse XML tree and find <witness> elements
     mytree = msTree(siglum)
     witnesses = mytree.tree.findall('.//t:%s' % ('witness'), myconst.ns)
-    juxtaSigla = [{'juxtaSiglum': witness.get(myconst.xml_ns + 'id'), 'element': witness} for witness in witnesses]
+    juxtaSigla = [
+        {'juxtaSiglum': witness.get(myconst.xml_ns + 'id'),
+         'element': witness}
+        for witness in witnesses
+    ]
 
-    # Search which <witness> represents the print edition ('bonetti' or 'garufi')
-    juxtaPrintSiglum = 'unknown' 
+    # Search which <witness> represents the print edition
+    # ('bonetti' or 'garufi'):
+    juxtaPrintSiglum = 'unknown'
     for myWitness in juxtaSigla:
-        if printEdition in myWitness['element'].text.lower(): # e.g.: if 'garufi' in... or if 'bonetti' in...
+        # If 'garufi' in... or if 'bonetti' in...
+        if printEdition in myWitness['element'].text.lower():
             juxtaPrintSiglum = myWitness['juxtaSiglum']
-            myWitness['mySiglum'] = printSiglum # Associate my print siglum (e.g. 'g') to the JuxtaCommons siglum (e.g. 'wit-41657')
-
+            # Associate my print siglum (e.g. 'g') to the JuxtaCommons siglum
+            # (e.g. 'wit-41657'):
+            myWitness['mySiglum'] = printSiglum
 
     # Check if the print <witness> has been found or not
     if juxtaPrintSiglum == 'unknown':
-        print('I haven\'t found what witness in %s corresponds to %s. Please include string «%s» in the text of one <witness> in %s' %
-                (siglum, printEdition.capitalize(), printEdition, siglum  ))
+        print('I haven\'t found what witness in %s corresponds to %s. \
+              Please include string «%s» in the text of one <witness> in %s' %
+              (siglum, printEdition.capitalize(), printEdition, siglum))
     else:
-        print('The JuxtaCommons-generated witness for %s is %s' % (printEdition.capitalize(), juxtaPrintSiglum) )
+        print('The JuxtaCommons-generated witness for %s is %s' %
+              (printEdition.capitalize(), juxtaPrintSiglum))
 
     for myWitness in juxtaSigla:
-        #if myWitness['mySiglum'] is None:
+        # if myWitness['mySiglum'] is None:
         if 'mySiglum' not in myWitness:
-           myWitness['mySiglum'] = msSiglum # Associate my MS siglum (e.g. 'a') to the JuxtaCommons siglum (e.g. 'wit-41658')
+            # Associate my MS siglum (e.g. 'a') to the JuxtaCommons siglum
+            # (e.g. 'wit-41658'):
+            myWitness['mySiglum'] = msSiglum
 
     # Replace value in <witness xml:id...>
     for witness in witnesses:
@@ -62,7 +86,32 @@ def postProcessJuxtaCommonsFile (siglum, printEdition = 'garufi', printSiglum = 
             rdgSiglum = rdgSiglum.replace(s['juxtaSiglum'], s['mySiglum'])
         rdg.set('wit', rdgSiglum)
 
-    # Replace file
-    mytree.tree.write(xmlfile, encoding='UTF-8', method='xml', pretty_print=True, xml_declaration=True)
+    ######################################
+    # Remove empty <p> wrapping all text #
+    ######################################
+    body = mytree.tree.find('.//{http://www.tei-c.org/ns/1.0}body')
+    par = body.find('.//{http://www.tei-c.org/ns/1.0}p')
+    for child in par:
+        body.append(child)
+    body.remove(par)
 
-postProcessJuxtaCommonsFile ('m', 'garufi', 'g', 'a')
+    print(mytree.siglum, len(par))
+
+    # Replace file
+    mytree.tree.write(xmlfile, encoding='UTF-8', method='xml',
+                      pretty_print=True, xml_declaration=True)
+
+
+######################
+# EXECUTE FUNCTIONS  #
+######################
+
+
+'''
+postProcessJuxtaCommonsFile(siglum='m1', printEdition='garufi',
+                            printSiglum='g', msSiglum='a')
+postProcessJuxtaCommonsFile(siglum='m1-short', printEdition='garufi',
+                            printSiglum='g', msSiglum='a')
+'''
+postProcessJuxtaCommonsFile(siglum='m2', printEdition='bonetti',
+                            printSiglum='b', msSiglum='a')
