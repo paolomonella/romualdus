a29dfc6f8ce5395323c2fe10cfcbab460ce0ee9b
Paolo Monella <paolo.monella@gmx.net>
2020-03-06 12:56:57 +0100

    Make PEP8 compliant

--- python/visualization.py
+++ python/visualization.py
@@ -3,20 +3,27 @@
 
 import os
 
-from lint import tosLint 
+from lint import tosLint
 from output import writeToMultiMSTableFile
 from detect_combinations import detectCommonAbbrCombinations
 
 os.system('clear')
 
-mss = ['a', 'b', 'c']     # The sigla of the three manuscripts (in the future, it might include 'd' or others)
+mss = ['a-1and2unified', 'b', 'c']     # The sigla of the three manuscripts
 
 for x in mss:
     tosLint(x)
 
-for mscombi in [  ['a', 'b', 'c'],  ['a', 'b'],  ['a', 'c'],  ['b', 'c'],  ['a'],  ['b'],  ['c'] ]:
-    for layercombi in [     ['gl'],     ['al'],     ['gl', 'al']    ]:
+for mscombi in [['a-1and2unified', 'b', 'c'],
+                ['a-1and2unified', 'b'],
+                ['a-1and2unified', 'c'],
+                ['b', 'c'],
+                ['a-1and2unified'],
+                ['b'],
+                ['c']]:
+    for layercombi in [['gl'],     ['al'],     ['gl', 'al']]:
         writeToMultiMSTableFile(mscombi, layercombi, wbaretext=False)
 
 for m in mss:
-    detectCommonAbbrCombinations(m, quiet=False) # Choose False if you want a full report of all <abbr>s
+    # Choose False if you want a full report of all <abbr>s:
+    detectCommonAbbrCombinations(m, quiet=False)
