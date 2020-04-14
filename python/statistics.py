60f01dc1ba44c8749bcfc329742f4caaa0b9986d
Paolo Monella <paolo.monella@gmx.net>
2020-04-15 00:40:33 +0200

    Add statistics

--- python/statistics.py
+++ python/statistics.py
@@ -21,6 +21,9 @@ def insertnum(ancestor, number, n):
         then change <num n="s-subst"/> to <num n="s-subst">4</seg> '''
 
     e = ancestor.find('.//t:num[@n="%s"]' % n, ns)
+    if e is None:
+        print(('Subtype {} not found'
+              '').format(n))
     number_str = str(number)
     e.text = number_str
     e.set('value', number_str)
@@ -32,7 +35,6 @@ def recalculate_statistics():
     template_path = '%s%s' % (xmlpath, stats_template)
     template_tree = etree.parse(template_path)
     div = template_tree.find('.//t:div[@n="statistics"]', ns)
-    print(div)
 
     # Parse stats_filled file
     filled_path = '%s%s' % (xmlpath, stats_filled)
@@ -117,7 +119,6 @@ def recalculate_statistics():
     # Subtypes of type substantive
 
     s_num_num = len(tree.findall('.//t:app[@subtype="%s"]' % 'num-num', ns))
-    print(s_num_num)
     insertnum(div, s_num_num, 's-num-num')
 
     s_perc_num_num = "%.1f" % (s_num_num / v * 100)
@@ -127,165 +128,145 @@ def recalculate_statistics():
                                        % 'missing-in-ms', ns))
     insertnum(div, s_missing_in_ms, 's-missing-in-ms')
 
-    '''
-    s_perc_missing_in_ms
-    's-perc-missing-in-ms'
+    s_perc_missing_in_ms = "%.1f" % (s_missing_in_ms / v * 100)
+    insertnum(div, s_perc_missing_in_ms, 's-perc-missing-in-ms')
 
     s_missing_in_print = len(tree.findall('.//t:app[@subtype="%s"]'
-        % 's-missing-in-print', ns))
+                                          % 'missing-in-print', ns))
     insertnum(div, s_missing_in_print, 's-missing-in-print')
 
-    s_perc_missing_in_print
-    's-perc-missing-in-print'
+    s_perc_missing_in_print = "%.1f" % (s_missing_in_print / v * 100)
+    insertnum(div, s_perc_missing_in_print, 's-perc-missing-in-print')
 
-    s_fs = len(tree.findall('.//t:app[@subtype="%s"]' % 's-fs', ns))
+    s_fs = len(tree.findall('.//t:app[@subtype="%s"]' % 'fs', ns))
     insertnum(div, s_fs, 's-fs')
 
-    s_perc_fs
-    's-perc-fs'
+    s_perc_fs = "%.1f" % (s_fs / v * 100)
+    insertnum(div, s_perc_fs, 's-perc-fs')
 
     # Subsubtypes of subtype orthographic
 
-    s_y = len(tree.findall('.//t:app[@subtype="%s"]' % 's-y', ns))
+    s_y = len(tree.findall('.//t:app[@subtype="%s"]' % 'y', ns))
     insertnum(div, s_y, 's-y')
 
-    s_perc_y
-    's-perc-y'
+    s_perc_y = "%.1f" % (s_y / v * 100)
+    insertnum(div, s_perc_y, 's-perc-y')
 
-    s_v = len(tree.findall('.//t:app[@subtype="%s"]' % 's-v', ns))
-    insertnum(div, s_v, 's-v')
-
-    s_perc_v
-    's-perc-v'
-
-    s_j = len(tree.findall('.//t:app[@subtype="%s"]' % 's-j', ns))
-    insertnum(div, s_j, 's-j')
-
-    s_perc_j
-    's-perc-j'
-
-    s_ae = len(tree.findall('.//t:app[@subtype="%s"]' % 's-ae', ns))
+    s_ae = len(tree.findall('.//t:app[@subtype="%s"]' % 'ae', ns))
     insertnum(div, s_ae, 's-ae')
 
-    s_perc_ae
-    's-perc-ae'
+    s_perc_ae = "%.1f" % (s_ae / v * 100)
+    insertnum(div, s_perc_ae, 's-perc-ae')
 
-    s_h = len(tree.findall('.//t:app[@subtype="%s"]' % 's-h', ns))
+    s_h = len(tree.findall('.//t:app[@subtype="%s"]' % 'h', ns))
     insertnum(div, s_h, 's-h')
 
-    s_perc_h
-    's-perc-h'
+    s_perc_h = "%.1f" % (s_h / v * 100)
+    insertnum(div, s_perc_h, 's-perc-h')
 
-    s_n_pn = len(tree.findall('.//t:app[@subtype="%s"]' % 's-n-pn', ns))
-    insertnum(div, s_n, 's-n-pn')
-
-    s_perc_n_pn
-    's-perc-n-pn'
-
-    s_hi_y = len(tree.findall('.//t:app[@subtype="%s"]' % 's-hi-y', ns))
+    s_hi_y = len(tree.findall('.//t:app[@subtype="%s"]' % 'hi-y', ns))
     insertnum(div, s_hi_y, 's-hi-y')
 
-    s_perc_hi-y
-    's-perc-hi-y'
+    s_perc_hi_y = "%.1f" % (s_hi_y / v * 100)
+    insertnum(div, s_perc_hi_y, 's-perc-hi-y')
 
-    s_num-word = len(tree.findall('.//t:app[@subtype="%s"]'
-    % 's-num-word', ns))
-    insertnum(div, s_num, 's-num-word')
+    s_num_word = len(tree.findall('.//t:app[@subtype="%s"]'
+                                  % 'num-word', ns))
+    insertnum(div, s_num_word, 's-num-word')
 
-    s_perc_num-word
-    's-perc-num-word'
+    s_perc_num_word = "%.1f" % (s_num_word / v * 100)
+    insertnum(div, s_perc_num_word, 's-perc-num-word')
 
-    s_case = len(tree.findall('.//t:app[@subtype="%s"]' % 's-case', ns))
+    s_case = len(tree.findall('.//t:app[@subtype="%s"]' % 'case', ns))
     insertnum(div, s_case, 's-case')
 
-    s_perc_case
-    's-perc-case'
+    s_perc_case = "%.1f" % (s_case / v * 100)
+    insertnum(div, s_perc_case, 's-perc-case')
 
-    s_tc = len(tree.findall('.//t:app[@subtype="%s"]' % 's-tc', ns))
+    s_tc = len(tree.findall('.//t:app[@subtype="%s"]' % 'tc', ns))
     insertnum(div, s_tc, 's-tc')
 
-    s_perc_tc
-    's-perc-tc'
+    s_perc_tc = "%.1f" % (s_tc / v * 100)
+    insertnum(div, s_perc_tc, 's-perc-tc')
 
-    s_double = len(tree.findall('.//t:app[@subtype="%s"]' % 's-double', ns))
+    s_double = len(tree.findall('.//t:app[@subtype="%s"]' % 'double', ns))
     insertnum(div, s_double, 's-double')
 
-    s_perc_double
-    's-perc-double'
+    s_perc_double = "%.1f" % (s_double / v * 100)
+    insertnum(div, s_perc_double, 's-perc-double')
 
-    s_ph = len(tree.findall('.//t:app[@subtype="%s"]' % 's-ph', ns))
+    s_ph = len(tree.findall('.//t:app[@subtype="%s"]' % 'ph', ns))
     insertnum(div, s_ph, 's-ph')
 
-    s_perc_ph
-    's-perc-ph'
+    s_perc_ph = "%.1f" % (s_ph / v * 100)
+    insertnum(div, s_perc_ph, 's-perc-ph')
 
-    s_cq = len(tree.findall('.//t:app[@subtype="%s"]' % 's-cq', ns))
+    s_cq = len(tree.findall('.//t:app[@subtype="%s"]' % 'cq', ns))
     insertnum(div, s_cq, 's-cq')
 
-    s_perc_cq
-    's-perc-cq'
+    s_perc_cq = "%.1f" % (s_cq / v * 100)
+    insertnum(div, s_perc_cq, 's-perc-cq')
 
-    s_mpn = len(tree.findall('.//t:app[@subtype="%s"]' % 's-mpn', ns))
+    s_mpn = len(tree.findall('.//t:app[@subtype="%s"]' % 'mpn', ns))
     insertnum(div, s_mpn, 's-mpn')
 
-    s_perc_mpn
-    's-perc-mpn'
+    s_perc_mpn = "%.1f" % (s_mpn / v * 100)
+    insertnum(div, s_perc_mpn, 's-perc-mpn')
 
-    s_tz = len(tree.findall('.//t:app[@subtype="%s"]' % 's-tz', ns))
+    s_tz = len(tree.findall('.//t:app[@subtype="%s"]' % 'tz', ns))
     insertnum(div, s_tz, 's-tz')
 
-    s_perc_tz
-    's-perc-tz'
+    s_perc_tz = "%.1f" % (s_tz / v * 100)
+    insertnum(div, s_perc_tz, 's-perc-tz')
 
-    s_cz = len(tree.findall('.//t:app[@subtype="%s"]' % 's-cz', ns))
+    s_cz = len(tree.findall('.//t:app[@subtype="%s"]' % 'cz', ns))
     insertnum(div, s_cz, 's-cz')
 
-    s_perc_cz
-    's-perc-cz'
+    s_perc_cz = "%.1f" % (s_cz / v * 100)
+    insertnum(div, s_perc_cz, 's-perc-cz')
 
-    s_ck = len(tree.findall('.//t:app[@subtype="%s"]' % 's-ck', ns))
+    s_ck = len(tree.findall('.//t:app[@subtype="%s"]' % 'ck', ns))
     insertnum(div, s_ck, 's-ck')
 
-    s_perc_ck
-    's-perc-ck'
+    s_perc_ck = "%.1f" % (s_ck / v * 100)
+    insertnum(div, s_perc_ck, 's-perc-ck')
 
-    s_word-segmentation = len(tree.findall('.//t:app[@subtype="%s"]'
-        % 's-word-segmentation', ns))
-    insertnum(div, s_word, 's-word-segmentation')
+    s_word_segmentation = len(tree.findall('.//t:app[@subtype="%s"]'
+                                           % 'word-segmentation', ns))
+    insertnum(div, s_word_segmentation, 's-word-segmentation')
 
-    s_perc_word-segmentation
-    's-perc-word-segmentation'
+    s_perc_word_segmentation = "%.1f" % (s_word_segmentation / v * 100)
+    insertnum(div, s_perc_word_segmentation, 's-perc-word-segmentation')
 
-    s_ch = len(tree.findall('.//t:app[@subtype="%s"]' % 's-ch', ns))
+    s_ch = len(tree.findall('.//t:app[@subtype="%s"]' % 'ch', ns))
     insertnum(div, s_ch, 's-ch')
 
-    s_perc_ch
-    's-perc-ch'
+    s_perc_ch = "%.1f" % (s_ch / v * 100)
+    insertnum(div, s_perc_ch, 's-perc-ch')
 
-    s_pb = len(tree.findall('.//t:app[@subtype="%s"]' % 's-pb', ns))
+    s_pb = len(tree.findall('.//t:app[@subtype="%s"]' % 'pb', ns))
     insertnum(div, s_pb, 's-pb')
 
-    s_perc_pb
-    's-perc-pb'
+    s_perc_pb = "%.1f" % (s_pb / v * 100)
+    insertnum(div, s_perc_pb, 's-perc-pb')
 
-    s_dm-mm = len(tree.findall('.//t:app[@subtype="%s"]' % 's-dm-mm', ns))
-    insertnum(div, s_dm, 's-dm-mm')
+    s_dm_mm = len(tree.findall('.//t:app[@subtype="%s"]' % 'dm-mm', ns))
+    insertnum(div, s_dm_mm, 's-dm-mm')
 
-    s_perc_dm-mm
-    's-perc-dm-mm'
+    s_perc_dm_mm = "%.1f" % (s_dm_mm / v * 100)
+    insertnum(div, s_perc_dm_mm, 's-perc-dm-mm')
 
-    s_mn = len(tree.findall('.//t:app[@subtype="%s"]' % 's-mn', ns))
+    s_mn = len(tree.findall('.//t:app[@subtype="%s"]' % 'mn', ns))
     insertnum(div, s_mn, 's-mn')
 
-    s_perc_mn
-    's-perc-mn'
+    s_perc_mn = "%.1f" % (s_mn / v * 100)
+    insertnum(div, s_perc_mn, 's-perc-mn')
 
-    s_ncx = len(tree.findall('.//t:app[@subtype="%s"]' % 's-ncx', ns))
+    s_ncx = len(tree.findall('.//t:app[@subtype="%s"]' % 'ncx', ns))
     insertnum(div, s_ncx, 's-ncx')
 
-    s_perc_ncx
-    's-perc-ncx'
-    '''
+    s_perc_ncx = "%.1f" % (s_ncx / v * 100)
+    insertnum(div, s_perc_ncx, 's-perc-ncx')
 
     # Write tree to output file stats_filled.xml
     # (warning: I'm overwriting the file)
