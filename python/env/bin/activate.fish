f3dd4af9d4675850dcf940a96302a0399b963b26
Paolo Monella <paolo.monella@gmx.net>
2020-05-06 18:34:42 +0200

    Reinstall virtual environment

--- python/env/bin/activate.fish
+++ python/env/bin/activate.fish
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(env) "            
+        if test -n "(env) "
             printf "%s%s" "(env) " (set_color normal)
         else
             # ...Otherwise, prepend env
